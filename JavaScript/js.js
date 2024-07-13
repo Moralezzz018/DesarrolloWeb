1733d88c2cc553bf8e718e553a876a658e5a99d4
Moralezzz018 <naha_moralesa55@gmail.com>
2024-07-13 09:20:04 -0600

    Intento de atomo, Introduccion a JavaScript

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -46,3 +46,51 @@ do {
     intControl --;
 }while (intControl > .0)
     
+//Areglos
+const arrNombre = [1, true, "Hola"];
+
+const arrPalabras = [["1","2","3","4"], ["5","6","7","8"]]
+for (let i=0;j<arrPalabras.length;i++){
+    for (let j=0;j<arrPalabras[i].length;j++)
+        console.log(`i:${i} j:{j} -> ${arrPalabras[i],[j]}`)
+}
+
+console.log("-------------------------------------------")
+
+/*arrPalabras.forEach((elm, i)=>{
+    ele.forEach(pal, j)=>{
+        console.log(`i:${i} j:{j} -> ${pal}`)
+    }
+});*/
+
+const arrNumeros = arrPalabras.map(
+    (elm)=> {
+        return elm.map((pal)=>{ return Number(pal)});
+    }
+);
+
+console.log('arrPlabras', arrPalabras)
+console.log('arrNumeros', arrNumeros)
+
+//Olog buscar que es
+const arrNonSorted = [15,8,10,100,20,12,33,500]
+const sortedArray = [...arrNonSorted].sort(
+    (objA, objB)=>{return objA-objB}
+);
+const rsortedArray = [...arrNonSorted].sort(
+    (objA, objB)=>{return objB-objA}
+);
+console.log(`sortedArray`, sortedArray);
+console.log(`rsortedArray`, rsortedArray);
+
+//filtro
+const lessThan30 = arrNonSorted.filter(
+    (obj)=>{
+        return obj < 30;
+    }
+);
+console.log(`lessThan30`, lessThan30);
+
+console.log(
+    arrNumeros.map(elm=>elm.join(',')).join('\n\r')
+)
